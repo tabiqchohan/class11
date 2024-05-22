b12dcc82362a865a432fe4e2b561bf145cf4371e
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-05-22 10:19:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var myProperties = {
+    name: "tabiq",
+    age: 24,
+    rollNo: 51230,
+    adress: {
+        ciy: "karachi",
+        country: "pakistan",
+        postNo: "123",
+    }
+};
+console.log(myProperties.adress.country);
+// const userPromt = "name"
+// console.log(myProperties[userPromt]);
---
+++ main.ts
@@ -0,0 +1,15 @@
+ const myProperties = {
+     name : "tabiq",
+     age : 24,
+     rollNo :51230,
+     adress :{
+        ciy : "karachi",
+        country : "pakistan",
+        postNo : "123",
+     }
+ }
+ console.log(myProperties.adress.country);
+// const userPromt = "name"
+// console.log(myProperties[userPromt]);
+
+
